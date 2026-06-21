71a95fc27507be3d3594fb7b850edfca0d2d8e26
AshwinGanta <sai881115@gmail.com>
2026-06-21 18:10:06 +0530

    doc_ai

--- doctor_ai_backend/settings.py
+++ doctor_ai_backend/settings.py
@@ -132,5 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-CORS_ALLOW_ALL_ORIGINS = True 
- 
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
